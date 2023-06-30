6440327e323cb06273f4516230c72e4ad7559e46
Amanmandal-M <amanmandal644@gmail.com>
2023-06-30 13:17:06 +0530

    Fifth assignment done

--- Set1/fifth.py
+++ Set1/fifth.py
@@ -2,6 +2,7 @@
 #     - *Input*: "Python"
 #     - *Output*: "nohtyP"
 
+# First Method
 def reverse_string(original_string):
     reversed_str = ""
     for char in original_string:
@@ -11,4 +12,12 @@ def reverse_string(original_string):
 # Test the function
 original_string = "Python"
 reversed_str = reverse_string(original_string)
+# print(reversed_str)
+
+
+# Second Method
+reversed_str = ""
+for char in original_string:
+    reversed_str = char + reversed_str
+
 print(reversed_str)
